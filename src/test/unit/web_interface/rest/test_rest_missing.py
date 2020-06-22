be0c9ac0814d1213d7d05047cc675f8cf162674b
TingHL <2840168867@qq.com>
2020-06-22 22:56:21 +0800

    删除

--- src/test/unit/web_interface/rest/test_rest_missing.py
+++
@@ -1,7 +0,0 @@
-def test_missing(test_app):
-    result = test_app.get('/rest/missing').json
-
-    assert 'missing_analyses' in result
-    assert result['missing_analyses'] == {'root_fw_uid': ['missing_child_uid']}
-    assert 'missing_files' in result
-    assert result['missing_files'] == {'parent_uid': ['missing_child_uid']}
